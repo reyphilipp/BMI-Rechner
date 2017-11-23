e6c00ebdb4fc0fb8f612780af86b6f394c58c8d6
reyphilipp <33811876+reyphilipp@users.noreply.github.com>
2017-11-23 08:05:23 +0100

    Update bmi.kt

--- BMI-Rechner/src/bmi.kt
+++ BMI-Rechner/src/bmi.kt
@@ -1,79 +1,69 @@
+/**
+ * Created by admin on 19.11.2017.
+ */
 data class Person(
-    val Koerpergewicht:Double,
-    val Koerpergroesse:Double,
-    val Geschlecht:Boolean,
-    var Bmi:Double=0.0
+        val Koerpergewicht: Double,
+        val Koerpergroesse: Double,
+        val Geschlecht: Boolean,
+        var Bmi: Double = 0.0
 )
+
 fun main(args: Array<String>) {
     val person = Person(
             Koerpergroesse = abfrageGewichtOderGroesse("Körpergrösse? in cm"),
             Koerpergewicht = abfrageGewichtOderGroesse("Körpergewicht? in kg"),
-            Geschlecht = abfrageGeschlecht("Welches Geschlecht bist Du? m oder w")            )
+            Geschlecht = abfrageGeschlecht("Welches Geschlecht bist Du? m oder w"))
     person.Bmi = berechnung(person)
-    println("Du hast "+berechnungAusgabe(person)+" ("+person.Bmi+")")
+    println("Du hast " + berechnungBmi(person) + " (" + person.Bmi + ")")
 }
 
-fun abfrageGewichtOderGroesse(frage:String):Double{
+fun abfrageGewichtOderGroesse(frage: String): Double {
     println(frage)
     val result = readLine()
     if (result != null)
-    try{
-        return result.toDouble()
-    }
-    catch (e: Exception){
-        println("Fehleingabe: $e")
-    }
+        try {
+            return result.toDouble()
+        } catch (e: Exception) {
+            println("Fehleingabe: $e")
+        }
     return 0.0
 }
 
-fun berechnung(person:Person):Double{
-    return person.Koerpergewicht / (Math.pow(person.Koerpergroesse/100,2.0))
+fun berechnung(person: Person): Double {
+    return person.Koerpergewicht / (Math.pow(person.Koerpergroesse / 100, 2.0))
 }
 
-fun abfrageGeschlecht(frage:String):Boolean{
+fun abfrageGeschlecht(frage: String): Boolean {
     println(frage)
     val result = readLine()
     if (result != null && result == "m") {
         return false
-    }
-    else if (result != null && result == "w"){
+    } else if (result != null && result == "w") {
         return true
-    }
-    else
-    {
+    } else {
         println("Fehleingabe Geschlecht")
         return true
     }
 }
 
-fun berechnungAusgabe(person:Person):String{
-    if (person.Geschlecht==false){
-        return berechnungBmiM(person.Bmi)
+fun berechnungBmi(person: Person): String {
+    var result: String = "Fehler berechnungBmi"
+    if (person.Geschlecht == false) {
+        when (person.Bmi) {
+            in 0..19 -> result = "Untergewicht"
+            in 20..24 -> result = "Normalgewicht"
+            in 25..29 -> result = "leichtes bis mittleres üUbergweicht"
+        }
+    } else {
+        when (person.Bmi) {
+            in 0..18 -> result = "Untergewicht"
+            in 19..23 -> result = "Normalgewicht"
+            in 24..29 -> result = "leichtes bis mittleres üUbergweicht"
+        }
     }
-    else if (person.Geschlecht==true){
-       return berechnungBmiW(person.Bmi)
+    when (person.Bmi) {
+        in 30..39 -> result = "schweres Übergewicht"
+        in 40..1000 -> result = "massive gefährdendes Übergewicht"
     }
-    else {
-        return "Fehler Berechnung BMI"
-    }
-}
-fun berechnungBmiM(bmi:Double):String{
-    when (bmi){
-        in 0..19 -> return "Untergewicht"
-        in 20..24 -> return "Normalgewicht"
-        in 25..29 -> return "leichtes bis mittleres üUbergweicht"
-        in 30..39 -> return "schweres Übergewicht"
-        in 40..1000 -> return "massive gefährdendes Übergewicht"
-        else -> return "Fehler Ausgabe String"
-    }
-}
-fun berechnungBmiW(bmi:Double):String{
-    when (bmi){
-        in 0..18 -> return "Untergewicht"
-        in 19..23 -> return "Normalgewicht"
-        in 24..29 -> return "leichtes bis mittleres üUbergweicht"
-        in 30..39 -> return "schweres Übergewicht"
-        in 40..1000 -> return "massive gefährdendes Übergewicht"
-        else -> return "Fehler Ausgabe String"
-    }
+    return result
 }
